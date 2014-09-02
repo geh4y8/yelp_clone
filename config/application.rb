efbd0e4f44311ca930a785136f69bed28dad3edc
geh4y8 <guy.halperin88@gmail.com>
2014-09-02 00:21:06 -0700

    fix application.rb again

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,6 @@ module YelpClone
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += [
-      'bgvideo.webm',
-      'bgvideo.mp4'
-    ]
+
   end
 end
