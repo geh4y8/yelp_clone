c8621b34b8784e2d33a040727c718ca842efc3e5
Guy Halperin <guy.halperin88@gmail.com>
2014-09-07 12:38:29 -0700

    change homepage style

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ActiveRecord::Base
   has_attached_file :restaurant_photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :restaurant_photo, :content_type => /\Aimage\/.*\Z/
-
+  validates :restaurant_photo, :attachment_presence => true
   has_many :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
 
   has_attached_file :user_photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :user_photo, :content_type => /\Aimage\/.*\Z/
+  validates :user_photo, :attachment_presence => true
 end
